be72f78bab37de8af4b0d9e53c608582bc65ac6d
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-07-31 22:57:56 -0500

    formatted how the pagination on the table looks

--- src/components/TablePagination.js
+++ src/components/TablePagination.js
@@ -1,9 +1,9 @@
+import { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
-import Select from 'react-select';
 
 const CustomTableFooter = styled.section`
   background: rgba(205, 205, 205, 0.9);
-  padding: 15px;
+  padding: 18px 15px;
   width: 100%;
 `;
 
@@ -23,15 +23,38 @@ const TablePagination = ({
   resultLimit,
   setResultLimit,
 }) => {
+  const [pageBtnValues, setPageBtnValues] = useState([]);
+
+  const getPageBtnValues = useCallback(() => {
+    if (totalPages > 7 && currentPage < 5) {
+      setPageBtnValues([1, 2, 3, 4, 5, '...', totalPages]);
+    } else if (totalPages > 7 && currentPage > totalPages - 4) {
+      setPageBtnValues([1, '...', totalPages - 3, totalPages - 2, totalPages - 1, totalPages]);
+    } else if (totalPages > 7) {
+      setPageBtnValues([1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPages]);
+    } else {
+      let result = [];
+      for (let i = 1; i <= totalPages; i++) {
+        result.push(i);
+      }
+      setPageBtnValues(result);
+    }
+  }, [currentPage, totalPages]);
+
+  useEffect(() => {
+    getPageBtnValues();
+  }, [totalPages, currentPage, getPageBtnValues]);
+
   return (
     <CustomTableFooter>
       <div className="row">
-        <div className="col">
-          Showing {entryStart} to {entryEnd} of {totalResults} results
+        <div className="col d-flex align-items-center">
+          <label className="mb-0">
+            Showing {entryStart} to {entryEnd} of {totalResults} results
+          </label>
         </div>
-        <div className="col">
+        <div className="col d-flex align-items-center justify-content-center">
           <label htmlFor="results-per-page-select">Results Per Page:</label>
-
           <ResultsSelect
             id="results-per-page-select"
             value={resultLimit}
@@ -43,16 +66,39 @@ const TablePagination = ({
             <option value={50}>50</option>
           </ResultsSelect>
         </div>
-        <nav className="col" aria-label="Table Page Navigation">
+        <nav className="col d-flex align-items-center justify-content-end" aria-label="Table Page Navigation">
           <ul className="pagination mb-0">
-            <li className="page-item">
-              <button className="page-link" aria-label="Previous">
+            <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
+              <button className="page-link" aria-label="Previous" onClick={() => setCurrentPage(currentPage - 1)}>
                 <span> &#60; </span>
                 <span className="visually-hidden">Previous</span>
               </button>
             </li>
+            {pageBtnValues.map((value, index) => {
+              if (value === currentPage) {
+                return (
+                  <li className="page-item active" key={`pagination-btn-${index}`}>
+                    <button className="page-link">{value}</button>
+                  </li>
+                );
+              } else if (value === '...') {
+                return (
+                  <li className="page-item disabled" key={`pagination-btn-${index}`}>
+                    <button className="page-link">{value}</button>
+                  </li>
+                );
+              } else {
+                return (
+                  <li className="page-item" key={`pagination-btn-${index}`}>
+                    <button onClick={() => setCurrentPage(value)} className="page-link">
+                      {value}
+                    </button>
+                  </li>
+                );
+              }
+            })}
             <li className="page-item">
-              <button className="page-link" aria-label="next">
+              <button className="page-link" aria-label="next" onClick={() => setCurrentPage(currentPage + 1)}>
                 <span> &#62; </span>
                 <span className="visually-hidden">Next</span>
               </button>
