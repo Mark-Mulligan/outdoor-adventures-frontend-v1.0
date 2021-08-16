82cb23e70e93998493c64cb0dcf8c48b8efbfc84
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-08-15 21:02:04 -0500

    contact info edits for mobile size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,27 +11,37 @@ const StyledLink = styled.a`
   text-decoration: none;
 `;
 
+const BoldFont = styled.span`
+  font-weight: 700;
+`;
+
 const Contact = ({ contactInfo, websiteUrl }) => {
   return (
     <ParkInfoSection id="contact">
       <h2>Contact Info</h2>
       <hr />
       <div className="row">
-        <div className="col-6">
+        <div className="col-md-6 col-12">
           {contactInfo.phoneNumbers
             .filter((number) => number.type === 'Voice')
             .map((result) => {
-              return <p key={result.phoneNumber}>Phone: {formatPhoneNum(result.phoneNumber)}</p>;
+              return (
+                <p key={result.phoneNumber}>
+                  <BoldFont>Phone:</BoldFont> {formatPhoneNum(result.phoneNumber)}
+                </p>
+              );
             })}
         </div>
-        <div className="col-6">
+        <div className="col-md-6 col-12">
           {contactInfo.emailAddresses.map((email) => (
-            <p key={email.emailAddress}> Email: {email.emailAddress}</p>
+            <p key={email.emailAddress}>
+              <BoldFont>Email: </BoldFont> {email.emailAddress}
+            </p>
           ))}
         </div>
-        <div className="col-6">
+        <div className="col-12">
           <p>
-            Website:{' '}
+            <BoldFont>Website: </BoldFont>
             <StyledLink href={websiteUrl} target="_blank" rel="noreferrer">
               {websiteUrl} <i className="fas fa-xs fa-external-link-alt"></i>
             </StyledLink>
